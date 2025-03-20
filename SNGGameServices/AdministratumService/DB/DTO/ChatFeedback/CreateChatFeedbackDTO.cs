a4117d9667c2f1fc1e92ce774de0cf688ed4b613
Gregory Bakhtin <greasha46@gmail.com>
2025-03-20 16:16:52 +0300

    add CreateChatFeedbackDTO

---
+++ SNGGameServices/AdministratumService/DB/DTO/ChatFeedback/CreateChatFeedbackDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AdministratumService.DB.DTO.ChatFeedback
+{
+    public class CreateChatFeedbackDTO
+    {
+        [Required(ErrorMessage = "Не было указано название заголовка чата")]
+        [StringLength(100, ErrorMessage = "Название заголовка чата не должно превышать 100 символов")]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Не был прикреплён пользователь к чату")]
+        public int UserId { get; set; }
+    }
+}
