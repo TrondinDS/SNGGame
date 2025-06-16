b4f2b78736a2b8191804911c7135081772415247
Gregory Bakhtin <greasha46@gmail.com>
2025-06-16 14:49:23 +0300

    add chatfeedback

---
+++ SNGGameServices/GetAwaitService/Services/ChatFeedbackService/Interfaces/IChatFeedbackService.cs
@@ -0,0 +1,14 @@
+﻿using Library.Generics.DB.DTO.DTOModelServices.AdministratumService.ChatFeedback;
+using Library.Generics.DB.DTO.DTOModelServices.UserService.User;
+
+namespace GetAwaitService.Services.ChatFeedbackService.Interfaces
+{
+    public interface IChatFeedbackService
+    {
+        Task<IEnumerable<ChatFeedbackDTO>?> GetAll();
+        Task<ChatFeedbackDTO?> GetById(Guid id);
+        Task<ChatFeedbackDTO?> Create(ChatFeedbackDTO userDto);
+        Task<bool> Update(ChatFeedbackDTO userDto);
+        Task<bool> Delete(Guid id);
+    }
+}
